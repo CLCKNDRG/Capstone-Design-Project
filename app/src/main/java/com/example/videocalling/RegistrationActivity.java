dbcd38482eafe41a9893db0dd7ecd54563dd9155
CLCKNDRG <virtineaurum@gmail.com>
2020-09-17 17:33:30 +0900

    Added User Registration Feature

--- app/src/main/java/com/example/videocalling/RegistrationActivity.java
+++ app/src/main/java/com/example/videocalling/RegistrationActivity.java
@@ -1,9 +1,12 @@
 package com.example.videocalling;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,10 +14,18 @@ import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthProvider;
 import com.hbb20.CountryCodePicker;
+
+import java.util.concurrent.TimeUnit;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -25,7 +36,7 @@ public class RegistrationActivity extends AppCompatActivity {
     private String checker = "", phoneNumber = "";
     private RelativeLayout relativeLayout;
 
-    private PhoneAuthProvider.OnVerificationStateChangedCallbacks callbacks;
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
     private FirebaseAuth mAuth;
     private String mVerificationId;
     private PhoneAuthProvider.ForceResendingToken mResendToken;
@@ -35,6 +46,9 @@ public class RegistrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        mAuth = FirebaseAuth.getInstance();
+        loadingBar = new ProgressDialog(this);
 
         //컴포넌트 지정
         phoneText = findViewById(R.id.phoneText);
@@ -49,21 +63,103 @@ public class RegistrationActivity extends AppCompatActivity {
         continueAndNextBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (continueAndNextBtn.getText().equals("확인") || checker.equals("비밀번호 전송됨")) {
+                if (continueAndNextBtn.getText().equals("확인") || checker.equals("인증번호 전송됨")) {
+                    //인증 확인 코드 검증
+                    String verificationCode = codeText.getText().toString();
+                    if(verificationCode.equals("")){
+                        Toast.makeText(RegistrationActivity.this, "인증번호를 입력해 주세요", Toast.LENGTH_SHORT).show();
+                    } else {
+                        loadingBar.setTitle("인증 확인");
+                        loadingBar.setMessage("확인 중입니다, 잠시만 기다려 주세요!");
+                        loadingBar.setCanceledOnTouchOutside(false);
+                        loadingBar.show();
 
+                        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(mVerificationId, verificationCode);
+                        signInWithPhoneAuthCredential(credential);
+                    }
                 } else {
                     phoneNumber = ccp.getFullNumberWithPlus();
-                    //Firebase를 활용하여 번호 확인
+                    //번호 확인 분기
                     if(!phoneNumber.equals("")) {
                         loadingBar.setTitle("번호 사용 가능 여부 확인");
                         loadingBar.setMessage("확인 중입니다, 잠시만 기다려 주세요!");
                         loadingBar.setCanceledOnTouchOutside(false);
                         loadingBar.show();
+
+                        //핸드폰 번호 인증 API
+                        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                                phoneNumber,
+                                60,
+                                TimeUnit.SECONDS,
+                                RegistrationActivity.this,
+                                mCallbacks
+                        );
+
                     }else {
                         Toast.makeText(RegistrationActivity.this, "유효하지 않은 번호입니다.", Toast.LENGTH_SHORT).show();
                     }
                 }
             }
         });
+
+        //Firebase를 통한 핸드폰 인증
+        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            @Override //인증 완료
+            public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+                signInWithPhoneAuthCredential(phoneAuthCredential);
+            }
+            @Override //인증 오류
+            public void onVerificationFailed(FirebaseException e) {
+                Toast.makeText(RegistrationActivity.this, "유효하지 않은 번호입니다.", Toast.LENGTH_SHORT).show();
+                loadingBar.dismiss();
+                relativeLayout.setVisibility(View.VISIBLE);
+
+                continueAndNextBtn.setText("계속");
+                codeText.setVisibility(View.VISIBLE);
+            }
+
+            //토큰 전송
+            @Override
+            public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                super.onCodeSent(s, forceResendingToken);
+
+                mVerificationId = s;
+                mResendToken = forceResendingToken;
+
+                relativeLayout.setVisibility(View.GONE);
+                checker = "인증번호 전송됨";
+                continueAndNextBtn.setText("확인");
+                codeText.setVisibility(View.VISIBLE);
+
+                loadingBar.dismiss();
+                Toast.makeText(RegistrationActivity.this, "인증번호가 전송되었습니다. 확인해주세요!", Toast.LENGTH_SHORT).show();
+            }
+        };
+    }
+
+    //핸드폰 인증 과정
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            loadingBar.dismiss();
+                            Toast.makeText(RegistrationActivity.this, "인증이 확인되었습니다!", Toast.LENGTH_SHORT).show();
+                            sendUserToMainActivity();
+                        } else {
+                            loadingBar.dismiss();
+                            String e = task.getException().toString();
+                            Toast.makeText(RegistrationActivity.this, "오류코드: " + e, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    //메인 화면으로 인증 결과를 보냄
+    private void sendUserToMainActivity(){
+        Intent intent = new Intent(RegistrationActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
